4c477febcc98bb223436b98b6fbd82ec573f21cf
jingxi <774874572@qq.com>
2021-09-10 07:07:15 +0800

    改造notes为FormItem

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-    <label class="notes">
-      <span class="name">备注</span>
+    <label class="formItem">
+      <span class="name">{{this.fieldName}}</span>
       <input type="text"
-             v-model="value"
-             placeholder="请输入备注">
+             :value="value"
+             @input="onValueChanged($event.target.value)"
+             :placeholder="this.placeholder">
     </label>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Watch} from 'vue-property-decorator';
-
+import {Component, Prop, Watch} from 'vue-property-decorator';
 @Component
-export default class Notes extends Vue {
-  value = '';
-
+export default class FormItem extends Vue {
+  @Prop({default: ''}) readonly value!: string;
+  @Prop({required: true}) fieldName!: string;
+  @Prop() placeholder?: string;
   @Watch('value')
   onValueChanged(value: string) {
     this.$emit('update:value', value);
@@ -25,19 +26,16 @@ export default class Notes extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.notes {
+.formItem {
   font-size: 14px;
-  background: #f5f5f5;
   padding-left: 16px;
   display: flex;
   align-items: center;
-
   .name {
     padding-right: 16px;
   }
-
   input {
-    height: 64px;
+    height: 40px;
     flex-grow: 1;
     background: transparent;
     border: none;
