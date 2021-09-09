1012b2f84eb7135fccfb23d77d0c4f4f4cff6586
jingxi <774874572@qq.com>
2021-09-09 10:53:49 +0800

    收集Money.vue各组件数据

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -3,7 +3,7 @@
     <ul class="types">
       <li :class="type === '-' && 'selected'"
           @click="selectType('-')">支出
-<!--          如果type === '-'，则class等于selected-->
+        <!--          如果type === '-'，则class等于selected-->
       </li>
       <li :class="type === '+' && 'selected'"
           @click="selectType('+')">收入
@@ -13,38 +13,45 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+import Vue from 'vue';
+import {Component, Watch} from 'vue-property-decorator';
 
-  @Component//修饰器的作用：type会自动被处理成data，selectType会自动被处理成methods
-  export default class Types extends Vue {
-    type = '-'; // '-'表示支出，'+'表示收入
-    selectType(type: string) {
-      if (type !== '-' && type !== '+') {
-        throw new Error('type is unknown');
-      }
-      this.type = type;
+@Component//修饰器的作用：type会自动被处理成data，selectType会自动被处理成methods
+export default class Types extends Vue {
+  type = '-'; // '-'表示支出，'+'表示收入
+  selectType(type: string) {
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
     }
+    this.type = type;
   }
+
+  @Watch('type')
+  onTypeChanged(value: string) {
+    this.$emit('update:value', value);
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-  .types {
-    background: #fde3cc;
+.types {
+  background: #fde3cc;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+
+  > li {
+    width: 50%;
+    height: 64px;
     display: flex;
-    text-align: center;
-    font-size: 24px;
-    > li {
-      width: 50%;
-      height: 64px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: relative;
-      &.selected{
-        background: #ffb13d;
-      }
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &.selected {
+      background: #ffb13d;
     }
   }
+}
 
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,25 +1,36 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad/>
-    <Types/>
-    <Notes/>
-    <Tags :data-source.sync="tags"/>
+    <NumberPad @update:value="onUpdateAmount"/>
+    <Types @update:value="onUpdateType"/>
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue'
 import NumberPad from '@/components/Money/NumberPad.vue'
 import Types from '@/components/Money/Types.vue'
 import Notes from '@/components/Money/Notes.vue'
 import Tags from '@/components/Money/Tags.vue'
+import {Component} from 'vue-property-decorator';
 
-export default {
-  name: 'Money',
-  components: {Tags, Notes, Types, NumberPad},
-  data() {
-    return {
-      tags: ['衣服', '吃饭', '交通', '水电', '房租', '彩妆']
-    }
+@Component({
+  components: {Tags, Notes, Types, NumberPad}
+})
+export default class Money extends Vue{
+  tags=['衣服', '吃饭', '交通', '水电', '房租', '彩妆'];
+  onUpdateTags(value:string[]){
+    console.log(value)
+  }
+  onUpdateNotes(value:string){
+    console.log(value)
+  }
+  onUpdateType(value:string){
+    console.log(value);
+  }
+  onUpdateAmount(value:string){
+    console.log(value);
   }
 }
 </script>
