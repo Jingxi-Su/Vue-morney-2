ab27d4e53abdc2949d845527e8f9e2213810630e
jingxi <774874572@qq.com>
2021-09-14 00:44:14 +0800

    Tags.vue使用Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -18,15 +18,18 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 
 @Component({
-  computed:{
-    tagList(){
-      //return this.$store.fetchTags();
-      return []
+  computed: {
+    tagList() {
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit('fetchTags');
+  }
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -43,7 +46,7 @@ export default class Tags extends Vue {
     if (!name) {
       return window.alert('标签名不能为空');
     }
-    //store.createTag(name);
+    this.$store.commit('createTag', name);
   }
 }
 </script>
